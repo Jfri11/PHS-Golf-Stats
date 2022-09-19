17af53258212672a3e651bcecd0f81225c25c546
Will Baysingar <wbaysingar@gmail.com>
2022-09-19 10:39:04 -0500

    Initial Commit

---
+++ PHS Golf Stats/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PHS Golf Stats
+//
+//  Created by Jack H. Friar on 8/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var NameTextField: UITextField!
+    @IBOutlet weak var HundredTextField: UITextField!
+    @IBOutlet weak var ScoreTextField: UITextField!
+    @IBOutlet weak var SneakTextField: UITextField!
+    @IBOutlet weak var FairwayTextField: UITextField!
+    @IBOutlet weak var GreensTextField: UITextField!
+    @IBOutlet weak var PuttTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func FinalButton(_ sender: UIButton) {
+    }
+    
+}
+
